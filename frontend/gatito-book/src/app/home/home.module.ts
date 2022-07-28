693201fbed63680b5e9a6245e7568bfb259efcca
Glacy <glacy.kelly3@gmail.com>
2022-07-28 20:00:19 -0300

    criando login component e rota

--- frontend/gatito-book/src/app/home/home.module.ts
+++ frontend/gatito-book/src/app/home/home.module.ts
@@ -3,14 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, LoginComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
   ],
-  exports: [HomeComponent]
+  exports: [HomeComponent, LoginComponent]
 })
 export class HomeModule { }
