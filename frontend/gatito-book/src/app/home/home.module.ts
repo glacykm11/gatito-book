554c4f478a319507c19475dc61572c80d837352d
Glacy <glacy.kelly3@gmail.com>
2022-07-28 21:52:42 -0300

    validação do formulário

--- frontend/gatito-book/src/app/home/home.module.ts
+++ frontend/gatito-book/src/app/home/home.module.ts
@@ -5,12 +5,14 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
+import { MensagemModule } from '../componentes/mensagem/mensagem.module';
 
 @NgModule({
   declarations: [HomeComponent, LoginComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,
+    MensagemModule,
     FormsModule
   ],
   exports: [HomeComponent, LoginComponent]
