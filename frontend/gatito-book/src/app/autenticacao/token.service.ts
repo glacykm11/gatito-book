31bfbff4639e532588d52aa34ca0c554126512fa
Glacy <glacy.kelly3@gmail.com>
2022-08-05 19:52:47 -0300

    implementando métodos no token-service

--- frontend/gatito-book/src/app/autenticacao/token.service.ts
+++ frontend/gatito-book/src/app/autenticacao/token.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
 
+const KEY = 'token';
+
 @Injectable({
   providedIn: 'root'
 })
 export class TokenService {
+ 
+  retornarToken() {
+    return localStorage.getItem(KEY) ?? '';
+  }
 
-  constructor() { }
+  salvarToken(token: string) {
+    localStorage.setItem(KEY, token);
+  }
+
+  excluirToken() {
+    localStorage.removeItem(KEY);
+  }
+
+  possuiToken() {
+    return !!this.retornarToken();
+  }
 }
